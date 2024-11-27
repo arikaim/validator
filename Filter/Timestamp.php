403bc42fe1318686562d8c749f97067c760d40e4
Arikaim <info@arikaim.com>
2024-11-27 12:54:44 +0200

    =

---
+++ Filter/Timestamp.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Arikaim
+ *
+ * @link        http://www.arikaim.com
+ * @copyright   Copyright (c)  Konstantin Atanasov <info@arikaim.com>
+ * @license     http://www.arikaim.com/license
+ * 
+ */
+namespace Arikaim\Core\Validator\Filter;
+
+use Arikaim\Core\Validator\Filter;
+use Arikaim\Core\Utils\DateTime;
+
+/**
+ * Timestamp filter
+ */
+class Timestamp extends Filter
+{  
+    /**
+     * Filter value, return filtered value
+     *
+     * @param mixed $value
+     * @return mixed
+     */
+    public function filterValue($value) 
+    {       
+        return (empty($value) == true) ? 
+            null : 
+            DateTime::toTimestamp($value,$this->params[0] ?? null);
+    } 
+
+    /**
+     * Return filter type
+     *
+     * @return mixed
+     */
+    public function getType()
+    {       
+        return FILTER_CALLBACK;
+    }
+}
